19c10e7cf9aee0c61b4ee71d77bbbfacb66993c9
kenan.lejosne@bentley.com <Kenan.Lejosne@bentley.com>
2019-10-31 15:53:47 +0100

    creating logical device
    Change-Id: I6776ff1737a4e06da1f1dff84776f70f61c1230f

--- src/VulkanBaseContext.cpp
+++ src/VulkanBaseContext.cpp
@@ -124,6 +124,41 @@ void VulkanBaseContext::mainLoop()
 
 void VulkanBaseContext::createLogicalDevice()
 {
+	QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+	if (!indices.isComplete())
+		throw std::runtime_error("failed to fetch the required queues");
+
+	VkDeviceQueueCreateInfo queueCreateInfo = {};
+	queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+	queueCreateInfo.queueCount = 1;
+
+	float queuePriority = 1.0f;
+	queueCreateInfo.pQueuePriorities = &queuePriority;
+
+	VkPhysicalDeviceFeatures deviceFeatures = {};
+
+	VkDeviceCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	createInfo.pQueueCreateInfos = &queueCreateInfo;
+	createInfo.queueCreateInfoCount = 1;
+
+	createInfo.pEnabledFeatures = &deviceFeatures;
+	createInfo.enabledExtensionCount = 0;
+
+	if (enableValidationLayers) 
+	{
+		createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+		createInfo.ppEnabledLayerNames = validationLayers.data();
+	}
+	else 
+	{
+		createInfo.enabledLayerCount = 0;
+	}
+
+	if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+		throw std::runtime_error("failed to create logical device!");
+	}
 }
 
 void VulkanBaseContext::cleanup()
@@ -132,7 +167,7 @@ void VulkanBaseContext::cleanup()
 	if (enableValidationLayers) {
 		DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
 	}
-
+	vkDestroyDevice(device, nullptr);
 	vkDestroyInstance(instance, nullptr);
 
 	glfwDestroyWindow(window);
