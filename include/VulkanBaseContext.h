c290884f9f4f2a70059868720d15ea3229e04020
kenan.lejosne@bentley.com <Kenan.Lejosne@bentley.com>
2019-10-31 11:28:39 +0100

    starting some clean up and putting things inside classes
    Change-Id: I146599595b8ea1e92d01041c5e8ee386d0408e47

---
+++ include/VulkanBaseContext.h
@@ -0,0 +1,91 @@
+#pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <vulkan/vulkan.h>
+
+#include <vector>
+#include <iostream>
+#include <stdexcept>
+#include <functional>
+#include <cstdlib>
+#include <cstring>
+
+namespace
+{
+
+
+	VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+		const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger)
+	{
+		auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+		if (func != nullptr) {
+			return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+		}
+		else {
+			return VK_ERROR_EXTENSION_NOT_PRESENT;
+		}
+	}
+	void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+		auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+		if (func != nullptr) {
+			func(instance, debugMessenger, pAllocator);
+		}
+	}
+
+}
+class VulkanBaseContext
+{
+public:
+	void run();
+
+private:
+	GLFWwindow* window;
+
+	VkInstance instance;
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+	VkDebugUtilsMessengerEXT debugMessenger;
+
+	const int WIDTH = 800;
+	const int HEIGHT = 600;
+
+	const std::vector<const char*> validationLayers = {
+		"VK_LAYER_KHRONOS_validation"
+	};
+
+#ifdef NDEBUG
+	const bool enableValidationLayers = false;
+#else
+	const bool enableValidationLayers = true;
+#endif
+
+
+
+//--------------------------------------------------------------------------------
+	void initVulkan();
+	void initWindow();
+	void createInstance();
+	void setupDebugMessenger();
+	void pickPhysicalDevice();
+	void mainLoop();
+	void cleanup();
+
+	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+	std::vector<const char*> getRequiredExtensions();
+	bool checkValidationLayerSupport();
+	bool isDeviceSuitable(VkPhysicalDevice device);
+
+
+
+	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+		VkDebugUtilsMessageTypeFlagsEXT messageType,
+		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+		void* pUserData);
+
+	static VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+		const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
+
+	void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
+
+};
