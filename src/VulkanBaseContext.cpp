3746ec450a3f860e0d2a7d6e6e1f5fe939080c8d
Kenan Lejosne <kenan.lejosne@gmail.com>
2019-12-07 13:02:35 +0100

    loading the shaders

--- src/VulkanBaseContext.cpp
+++ src/VulkanBaseContext.cpp
@@ -1,5 +1,6 @@
 #include "VulkanBaseContext.h"
 
+#include <fstream>
 #include <algorithm>
 #include <iostream>
 #include <stdexcept>
@@ -9,6 +10,25 @@
 #include <cstdint>
 #include <set>
 
+namespace
+{
+	static std::vector<char> readFile(const std::string& filename) {
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+		if (!file.is_open()) {
+			throw std::runtime_error("failed to open file!");
+		}
+		size_t fileSize = (size_t)file.tellg();
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+
+		file.close();
+
+		return buffer;
+	}
+}
 
 void VulkanBaseContext::run()
 {
@@ -479,5 +499,6 @@ void VulkanBaseContext::createImageViews()
 
 void VulkanBaseContext::createGraphicsPipeline()
 {
-
+	auto vertShaderCode = readFile("shaders/vert.spv");
+	auto fragShaderCode = readFile("shaders/frag.spv");
 }
