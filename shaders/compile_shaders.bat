301cf2fd2b5fd1358ceef614e2b6e73d6790ace2
Lejosne <kenan.lejosne@margo-group.net>
2023-02-14 17:04:39 +0100

    Fixing the shader compiler invocation script

--- shaders/compile_shaders.bat
+++ shaders/compile_shaders.bat
@@ -1,11 +1,13 @@
 echo "compiling shader.vert..."
 
-%VULKAN_SDK%\Bin32\glslc.exe shader.vert -o vert.spv
-
+%VULKAN_SDK%\Bin\glslc.exe vert.vert -o vert.spv
 echo "Done."
+
+pause
+
 echo "compiling shader.frag..."
 
-%VULKAN_SDK%\Bin32\glslc.exe shader.frag -o frag.spv
+%VULKAN_SDK%\Bin\glslc.exe frag.frag -o frag.spv
 echo "Done."
 
 
